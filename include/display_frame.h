019902bf6357dad5a187d7c6449036394efabceb
amish0 <amishkumar562@gmail.com>
2023-08-21 00:26:02 +0530

    initial commit for display

--- include/display_frame.h
+++ include/display_frame.h
@@ -2,24 +2,41 @@
 #define DISPLAY_FRAME_H
 
 #include <iostream>
+#include <opencv2/opencv.hpp>
+#include <vector>
+#include <stdlib.h>
+#include <string.h>
+#include "config_reader.h"
+#include "config.h"
 
-struct cam_info_t
-{
-    char name[20];
-    char location[20];
-};
+#ifdef _WIN32
+    #include <Windows.h>
+    #define SYSTEM_TYPE "Windows"
+#elif __linux__
+    #include <X11/Xlib.h>
+    #define SYSTEM_TYPE "Linux"
+#else
+    #define SYSTEM_TYPE "Unknown"
+#endif
 
 
 class DisplayFrame
 {
     private:
-        int no_of_cameras;
         int rows;
         int cols;
+        int no_of_cameras;
+        int width;
+        int height;
+        int display_width;
+        int display_height;
+        std::queue<std::vector<cv::Mat>> frames;
+        cv::Mat display_frame;
+        string display_title = "Multi-Camera Tracking";
     public:
         DisplayFrame();
         ~DisplayFrame();
-        void displayFrame();
-
+        void update(std::vector<cv::Mat> r_frames);
+        void display();
 };
 #endif // DISPLAY_FRAME_H
--- src/display_frame.cpp
+++ src/display_frame.cpp
@@ -1,23 +1,74 @@
 #include "display_frame.h"
 
-// Path: display_frame.cpp
-// Compare this snippet from display_frame.h:
-// #include "display_frame.h"
-//
-
 DisplayFrame::DisplayFrame()
 {
-    no_of_cameras = 0;
-    rows = 0;
-    cols = 0;
+    no_of_cameras = configparameter.cam_num_;
+    width = configparameter.width_;
+    height = configparameter.height_;
+    if (SYSTEM_TYPE == "Windows")
+    {
+        display_width = GetSystemMetrics(SM_CXSCREEN);
+        display_height = GetSystemMetrics(SM_CYSCREEN);
+    }
+    else if (SYSTEM_TYPE == "Linux")
+    {
+        // Display* disp = XOpenDisplay(NULL);
+        // Screen*  scrn = DefaultScreenOfDisplay(disp);
+        // display_width = scrn->width;
+        // display_height = scrn->height;
+        display_width = 1920;
+        display_height = 1080;
+    }
+    else
+    {
+        display_width = 1920;
+        display_height = 1080;
+    }
+
+    switch (no_of_cameras)
+    {
+    case 1:
+        rows = 1;
+        cols = 1;
+        break;
+    case 2:
+        rows = 1;
+        cols = 2;
+        break;
+    case 3:
+    case 4:
+        rows = 2;
+        cols = 2;
+        break;
+    default:
+        exit(1);
+        break;
+    }
+    width = display_width / cols;
+    height = display_height / rows;
+    display_height = height * rows;
+    display_width = width * cols;
+    display_frame = cv::Mat(display_height, display_width, CV_8UC3, cv::Scalar(0, 0, 0));
 }
 
 DisplayFrame::~DisplayFrame()
 {
-    //dtor
+    
 }
 
-void DisplayFrame::displayFrame()
+void DisplayFrame::update(std::vector<cv::Mat> r_frames)
 {
-    std::cout << "Displaying frame" << std::endl;
+    frames.push_back(r_frames);
 }
+
+void DisplayFrame::display()
+{
+    if (frames.size()>0)
+    {
+        std::vector<cv::Mat> frame;
+        frame = frames.pop_back();
+        
+
+    }
+}
+
