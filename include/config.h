ae3995479e6575b7521e0c749335fc3920642f4e
amish0 <amishkumar562@gmail.com>
2023-08-20 17:56:58 +0530

    In Detection Structure we added extra parameter track_id

--- include/config.h
+++ include/config.h
@@ -4,13 +4,14 @@
 #include <opencv2/opencv.hpp>
 
 #define CONFIG_FILE "config.ini"
-#define MAX_AGES 30
+#define MAX_AGES 180
 #define MIN_HITS 5
-#define IOU_THRESHOLD 0.7
+#define IOU_THRESHOLD 0.3
 
 struct Detection
 {
     int class_id;
+    int track_id;
     float confidence;
     cv::Rect box;
 };
